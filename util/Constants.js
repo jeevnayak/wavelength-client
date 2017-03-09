deed0824a72c48562669b5d07b6be096fd0f020e
Rajeev Nayak <jeev.nayak@gmail.com>
2017-03-08 23:07:33 -0800

    move server config out of version control

--- util/Constants.js
+++ util/Constants.js
@@ -1,7 +1,7 @@
-const prod = true;
+import config from '../config.json';
 
 export default Constants = {
-  GraphQLUri: prod ? "https://jeev-wavelength.herokuapp.com/graphql" :
+  GraphQLUri: config.prod ? "https://jeev-wavelength.herokuapp.com/graphql" :
     "http://localhost:5000/graphql", // 10.0.3.2 for genymotion
   FbAppId: "1387424857934791",
   FbPermissions: ["public_profile", "user_friends"],
